45135d2db97e34a150fd094f336ae0eb28dfb29d
Marcos Barcia <marcos.b.iriarte@gmail.com>
2022-05-20 13:13:47 -0400

    first commit

---
+++ src/Components/NavBar/NavBr.jsx
@@ -0,0 +1,14 @@
+import './NavBr.scss';
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <ul className= "navContainer">
+            <li className = "list">Home</li>
+            <li className = "list">Categoria A</li>
+            <li className = "list">Categoria B</li>
+        </ul>
+    )
+}
+
+export default NavBar
